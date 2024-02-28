34dd8b468f6d7c1becbe6223585dc8e6cedd4b02
Pamela-Cas <pxcx611@gmail.com>
2024-02-27 21:37:30 -0300

    iniciando model hotel

--- src/TrybeHotel/Models/Hotel.cs
+++ src/TrybeHotel/Models/Hotel.cs
@@ -3,6 +3,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 // 1. Implemente as models da aplicação
-public class Hotel {
-
+public class Hotel
+{
+    public int HotelId { get; set; }
 }
--- src/TrybeHotel/Models/Room.cs
+++ src/TrybeHotel/Models/Room.cs
@@ -4,5 +4,5 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 // 1. Implemente as models da aplicação
 public class Room {
-
+   
 }
